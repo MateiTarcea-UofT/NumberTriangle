1735a723a977ff74804a7322057ba72ba3f1bd6b
MateiTarcea-UofT <matei.tarcea@mail.utoronto.ca>
2025-09-22 17:17:20 -0400

    Fixed retrieve to continue down NumberTree

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -91,11 +91,11 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        String[] split = path.split("(?!^)");
-        String[] rest = Arrays.copyOfRange(split, 1, split.length);
-        if (split[0].equals("l") || split[0].equals("r")) {
-            return retrieve(String.join(",", rest));
-        }else{
+        if (path.charAt(0) == 'l') {
+            return this.left.retrieve(path.substring(1));
+        } else if (path.charAt(0) == 'r') {
+            return this.right.retrieve(path.substring(1));
+        } else{
             return this.getRoot();
         }
     }
